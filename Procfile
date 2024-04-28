fc8401e477102565e9fb956d8de5ba258717b479
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-04-28 12:30:59 +0530

    ᴅᴇᴠɪʟ x ʙᴇᴛᴀ

---
+++ Procfile
@@ -0,0 +1,1 @@
+DEVIL: python -m main.py
---
+++ main.py
@@ -0,0 +1,72 @@
+#MIT License
+
+#Copyright (c) 2024 ᴋᴜɴᴀʟ
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import sys
+import glob
+import asyncio
+import logging
+import importlib
+import urllib3
+from pathlib import Path
+from config import KEX1, KEX2, KEX3, KEX4, KEX5, KEX6, KEX7, KEX8, KEX9, KEX10
+
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def load_plugins(plugin_name):
+    path = Path(f"DEVIL/modules/{plugin_name}.py")
+    spec = importlib.util.spec_from_file_location(f"DEVIL.modules.{plugin_name}", path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["DEVIL.modules." + plugin_name] = load
+    print("ᴅᴇᴠɪʟ ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ" + plugin_name)
+
+
+files = glob.glob("DEVIL/modules/*.py")
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("\ᴅᴇᴠɪʟ ɪꜱ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ 🥀")
+
+
+async def main():
+    await KEX1.run_until_disconnected()
+    await KEX2.run_until_disconnected()
+    await KEX3.run_until_disconnected()
+    await KEX4.run_until_disconnected()
+    await KEX5.run_until_disconnected()
+    await KEX6.run_until_disconnected()
+    await KEX7.run_until_disconnected()
+    await KEX8.run_until_disconnected()
+    await KEX9.run_until_disconnected()
+    await KEX10.run_until_disconnected()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
